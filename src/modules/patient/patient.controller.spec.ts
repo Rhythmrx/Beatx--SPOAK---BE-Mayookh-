e40f4a96b80f3616a3d4538e44501f777a3745c6
Mayookh <mayookh@beatly.ai>
2025-11-17 15:59:39 +0530

    updated testing files

--- src/modules/patient/patient.controller.spec.ts
+++ src/modules/patient/patient.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientController } from './patient.controller';
+import { PatientService } from './patient.service';
 
 describe('PatientController', () => {
   let controller: PatientController;
@@ -7,6 +8,16 @@ describe('PatientController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PatientController],
+      providers: [
+        {
+          provide: PatientService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<PatientController>(PatientController);
--- src/modules/patient/patient.service.spec.ts
+++ src/modules/patient/patient.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PatientService } from './patient.service';
+import { PatientUseCase } from './usecases/patient.usecase';
 
 describe('PatientService', () => {
   let service: PatientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PatientService],
+      providers: [PatientService,{
+        provide:PatientUseCase,
+        useValue:{
+         createPatient: jest.fn(),
+          getPatients: jest.fn(),
+          getPatientById: jest.fn(),
+        }
+      }],
     }).compile();
 
     service = module.get<PatientService>(PatientService);
